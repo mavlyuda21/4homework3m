b066af8911eb89358992f5c9b0d5cc6e3c68144f
sharshenovra <sharshenovr@icloud.com>
2023-03-17 15:57:29 +0600

    Initial Commit

---
+++ 4homework3m/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4homework3m
+//
+//  Created by Ruslan Sharshenov on 17/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
